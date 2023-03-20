5f07d58d0897124fc14b2cb5d0d7134aaee53683
Cameron Peck <cameronpeck317@gmail.com>
2023-03-20 21:27:39 +0000

    add handlekeydown function and check for enter keypress. On enter, do work. set the value back to an empty string after work is done

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { DataNode, root } from './data';
 
 const Tree: React.FC<DataNode> = ({ name, children }: DataNode) => {
+  const [value, setValue] = useState('')
+  
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log(value)
+      // do work
+      setValue('')
+    }
+  }
+
   return (
     <main className="pl-12">
       <div>
         <h4>{name}</h4>
-        <input className="border border-blue-500 rounded-md pl-2 pt-1 pb-1" type="text" placeholder='Add Child'/>
+        <input onChange={e => setValue(e.target.value)} value={value} onKeyDown={event => handleKeyDown(event)} className="border border-blue-500 rounded-md pl-2 pt-1 pb-1" type="text" placeholder='Add Child'/>
       </div>
       <div className="relative flex flex-col left-25 pl-12">
       {
