4a16ef1f8d7b7eb2b3eb701700c65246fd59941f
Cameron Peck <cameronpeck317@gmail.com>
2023-03-20 19:08:04 +0000

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const outputPath = path.join(__dirname, '/dist/');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: outputPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json']
+        },
+        use: 'ts-loader'
+      }
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'dist/index.html'
+    })
+  ]
+};
