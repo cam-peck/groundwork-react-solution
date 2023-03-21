04a30557f7139f3686c47433bba885456cbb3f51
Cameron Peck <cameronpeck317@gmail.com>
2023-03-21 21:56:34 +0000

    add draggable and droppable components from dnd package

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import Tree from './components/tree';
 import { root } from './data';
 import Header from './components/header';
+import { DndContext } from '@dnd-kit/core';
 
 export default function App() {
   return (
     <main className="max-w-6xl mx-auto">
       <Header />
-      <Tree {...root}/>
+      <DndContext>
+        <Tree {...root}/>
+      </DndContext>
     </main>
   )
 }
--- src/components/tree.tsx
+++ src/components/tree.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { DataNode } from '../data';
+import { Droppable } from './droppable';
+import { Draggable } from './draggable';
 
 export default function Tree({ name, children }: DataNode) {
 
@@ -7,8 +9,6 @@ export default function Tree({ name, children }: DataNode) {
   const [nodeChildren, setNodeChildren] = useState(children); 
   const [duplicateError, setDuplicateError] = useState(false);
 
-  const [isDown, setIsDown] = useState(false);
-  const [isDragging, setIsDragging] = useState(false);
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -32,7 +32,7 @@ export default function Tree({ name, children }: DataNode) {
   return (
     <section className="pl-12">
       {/* Node */}
-      <div onMouseDown={() => setIsDown(true)} onMouseMove={() => setIsDragging(true)}>
+      <div>
         <h4>{name}</h4>
         <input onChange={e => setValue(e.target.value)} value={value} onKeyDown={event => handleKeyDown(event)} className="border border-blue-500 rounded-md pl-2 pt-1 pb-1" type="text" placeholder='Add Child'/>
         { duplicateError && duplicateErrorText }
