61f7a4df9da69a50b231bdc31205a67449b08761
Cameron Peck <cameronpeck317@gmail.com>
2023-03-22 00:32:13 +0000

    implement drag and drop functionality within a level

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,12 @@ import React, { useState } from 'react';
 import Tree from './components/tree';
 import { root } from './data';
 import Header from './components/header';
-import { DndContext } from '@dnd-kit/core';
 
 export default function App() {
   return (
     <main className="max-w-6xl mx-auto">
       <Header />
-      <DndContext>
-        <Tree {...root}/>
-      </DndContext>
+      <Tree {...root}/>
     </main>
   )
 }
--- src/components/tree.tsx
+++ src/components/tree.tsx
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 import { DataNode } from '../data';
-import { Droppable } from './droppable';
-import { Draggable } from './draggable';
+import {
+  DndContext, 
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
+import {SortableItem} from './SortableItem';
 
 export default function Tree({ name, children }: DataNode) {
 
   const [value, setValue] = useState('')
-  const [nodeChildren, setNodeChildren] = useState(children); 
+  const [nodeChildren, setNodeChildren] = useState(children); // using this instead of dndkit [items, setItems] hook
   const [duplicateError, setDuplicateError] = useState(false);
 
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -27,6 +49,25 @@ export default function Tree({ name, children }: DataNode) {
     }
   }
 
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event
+    if (!over) return
+
+    const activeItem = nodeChildren.find((item) => item.name === active.id)
+    const overItem = nodeChildren.find((item) => item.name === over.id)
+
+    if (!activeItem || !overItem) {
+      return
+    }
+
+    const activeIndex = nodeChildren.findIndex((item) => item.name === active.id)
+    const overIndex = nodeChildren.findIndex((item) => item.name === over.id)
+
+    if (activeIndex !== overIndex) {
+      setNodeChildren((prev) => arrayMove(prev, activeIndex, overIndex))
+    }
+  }
+
   const duplicateErrorText = <p className="text-red-500 italic">A node with that name already exists.</p>
 
   return (
@@ -39,7 +80,11 @@ export default function Tree({ name, children }: DataNode) {
       </div>
       {/* Node's Children */}
       <div className="relative flex flex-col top-2 pl-12">
-        { nodeChildren.map((node: DataNode) => <Tree key={node.name} name={node.name} children={node.children} />) }
+        <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+          <SortableContext items={nodeChildren} strategy={verticalListSortingStrategy}>
+          { nodeChildren.map((node: DataNode) => <SortableItem key={node.name} id={node.name}><Tree key={node.name} name={node.name} children={node.children} /></SortableItem>) }
+          </SortableContext>
+        </DndContext>
       </div>
     </section>
   )
